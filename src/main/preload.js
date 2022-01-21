0b0d0ca94f881a9a7789152e8bcfbb5462d4b6f1
Domantas Stakionis <domantas.stakionis@ktu.edu>
2022-01-22 01:26:38 +0200

    Refactor preload script

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,31 +1,47 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electron', {
-  store: {
-    get(val) {
-      return ipcRenderer.sendSync('electron-store-get', val);
-    },
-    set(property, val) {
-      ipcRenderer.send('electron-store-set', property, val);
-    },
+const test = {
+  myPing() {
+    ipcRenderer.send('ipc-example', 'ping');
   },
-  ipcRenderer: {
-    myPing() {
-      ipcRenderer.send('ipc-example', 'ping');
-    },
-    on(channel, func) {
-      const validChannels = ['ipc-example'];
-      if (validChannels.includes(channel)) {
-        // Deliberately strip event as it includes `sender`
-        ipcRenderer.on(channel, (event, ...args) => func(...args));
-      }
-    },
-    once(channel, func) {
-      const validChannels = ['ipc-example'];
-      if (validChannels.includes(channel)) {
-        // Deliberately strip event as it includes `sender`
-        ipcRenderer.once(channel, (event, ...args) => func(...args));
-      }
-    },
+  on(channel, func) {
+    const validChannels = ['ipc-example'];
+    if (validChannels.includes(channel)) {
+      // Deliberately strip event as it includes `sender`
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
   },
-});
+  once(channel, func) {
+    const validChannels = ['ipc-example'];
+    if (validChannels.includes(channel)) {
+      // Deliberately strip event as it includes `sender`
+      ipcRenderer.once(channel, (event, ...args) => func(...args));
+    }
+  },
+};
+
+const store = {
+  get: (val) => {
+    return ipcRenderer.sendSync('electron-store-get', val);
+  },
+  set: (property, val) => ipcRenderer.send('electron-store-set', property, val),
+};
+
+const windowAction = {
+  close: () => ipcRenderer.send('action-close'),
+  maximize: () => ipcRenderer.send('action-maximize'),
+  minimize: () => ipcRenderer.send('action-minimize'),
+};
+
+// eslint-disable-next-line import/prefer-default-export
+const API = {
+  test,
+  store,
+  windowAction,
+};
+
+contextBridge.exposeInMainWorld('api', API);
+
+module.exports = {
+  API,
+};
