12e125f5ea42c05cfeb6bd123a59c59727e1c382
Domantas Stakionis <domantas.stakionis@ktu.edu>
2022-02-01 00:44:12 +0200

    Move theme initialization code

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,6 +3,10 @@ import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 import TitleBar from './components/TitleBar';
 
+// set theme from config
+let theme = window.api.store.get('theme');
+document.documentElement.className = theme;
+
 ReactDOM.render(
   <MemoryRouter>
     <div className="overflow-hidden">
