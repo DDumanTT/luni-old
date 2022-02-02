dd45baafdb88d32912a2b753c8cb7a3c571a5176
DDumanTT <haiwerss@gmail.com>
2022-02-02 18:17:33 +0200

    Fix button props

--- src/renderer/components/Button.tsx
+++ src/renderer/components/Button.tsx
@@ -1,8 +1,10 @@
 import { motion } from 'framer-motion';
+import { MouseEventHandler } from 'react';
 
 interface propsTypes {
   className?: string;
-  children: JSX.Element | JSX.Element[];
+  children: JSX.Element | JSX.Element[] | string;
+  onClick?: MouseEventHandler;
 }
 
 export default function Button(props: propsTypes) {
@@ -11,6 +13,7 @@ export default function Button(props: propsTypes) {
     <motion.div
       whileTap={{ scale: 0.95 }}
       className="bg-gradient-to-r from-indigo-800 to-indigo-600 hover:to-indigo-800 active:from-indigo-900 active:to-indigo-900 cursor-pointer rounded-lg box-shadow-lg"
+      onClick={props.onClick}
     >
       <div className={'flex items-center px-12 py-2' + ' ' + customClass}>
         {props.children}
